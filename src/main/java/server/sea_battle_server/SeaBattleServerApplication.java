20f97ffd452072e2a8387bf710f5bc8d17e2d100
Miroslav262 <mirostavchik235@gmail.com>
2026-04-03 12:00:45 +0300

    Creating repository

---
+++ src/main/java/server/sea_battle_server/SeaBattleServerApplication.java
@@ -0,0 +1,13 @@
+package server.sea_battle_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeaBattleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeaBattleServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/sea_battle_server/SeaBattleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package server.sea_battle_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeaBattleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
